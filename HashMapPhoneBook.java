55b5b99a56bc1556a92c34e1477177c62b7568ab
Makesya <trademakesya@gmail.com>
2023-07-08 23:26:24 +1000

    08072023

---
+++ HashMapPhoneBook.java
@@ -0,0 +1,140 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
+
+public class HashMapPhoneBook {
+    public static void main(String[] args) throws InterruptedException {
+        phoneDatabase();
+    }
+    public static void phoneDatabase() throws InterruptedException{
+        
+        try {
+            Map<String,LinkedList<Integer>> phoneBook = new TreeMap<>();
+
+        do {
+            clear();
+            System.out.println("1. Добавить");
+            System.out.println("2. Показать");
+            System.out.println("3. Удалить");
+            System.out.println("4. Выход");
+            Scanner scanner = new Scanner(System.in);
+            String inVar = scanner.nextLine();
+
+                
+      switch (inVar) {
+                case "1": add(phoneBook); break;
+                case "2": show(phoneBook); break;
+                case "3": delete(phoneBook); break;
+                case "4": exit();
+                }
+            } while (true);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }   
+                
+    }
+    private static void add(Map<String, LinkedList<Integer>> phoneBook) throws InterruptedException {
+        while(true){
+            try {
+                clear();
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Введите имя: ");
+            String name = scanner.nextLine().toLowerCase();
+            System.out.print("\nВведите номер телефона (без +7, 7, 8): ");
+            String numberPhone = scanner.nextLine();
+            if (numberPhone.isEmpty()) {
+                System.out.println("Номер телефона не может быть пустым...");
+                scanner.nextLine();
+                return;}
+
+            System.out.printf("\nКонтакт сохранен\n %s : %s\nНажмите для продолжения..", name, numberPhone);
+                
+                if(! phoneBook.containsKey(name)){
+                    phoneBook.put(name, new LinkedList<>());
+                    phoneBook.get(name).add(Integer.parseInt(numberPhone));
+                } else {
+                    phoneBook.get(name).add(Integer.parseInt(numberPhone));
+                }
+
+            scanner.nextLine();
+            return;
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+                Scanner scanner = new Scanner(System.in);
+                scanner.nextLine();
+            }
+
+        }
+    }
+    private static void show(Map<String, LinkedList<Integer>> phoneBook) throws InterruptedException {
+        clear();
+        if (phoneBook.isEmpty()) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Телефонная книга пуста");
+            System.out.println("Нажмите для продолжения..");
+            scanner.nextLine();
+            return;
+        } else {
+            for (Map.Entry<String, LinkedList<Integer>> contact : phoneBook.entrySet()) {
+                System.out.printf("%s: %s\n", contact.getKey(), contact.getValue().toString());}
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Нажмите для продолжения..");
+            scanner.nextLine();
+        }
+    }
+    private static void delete(Map<String, LinkedList<Integer>> phoneBook) {
+        try {
+            clear();
+        if (!phoneBook.isEmpty()){
+            for (Map.Entry<String, LinkedList<Integer>> contact : phoneBook.entrySet()) {
+                System.out.printf("%s ", contact.getKey());}
+        }
+        System.out.print("\nВведите имя контакта: ");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        clear();
+        if (! phoneBook.containsKey(name)){
+            clear();
+            System.out.println("Такого контакта не существует..");
+            scanner.nextLine();
+            return;
+        }
+        
+        System.out.printf("Какой номер удалить у %s:\n", name);
+        for (int i = 0; i < phoneBook.get(name).size(); i++) {
+            System.out.printf("%d. %d\n", i, phoneBook.get(name).get(i));
+        }
+        System.out.println("99. Удалить все");
+        System.out.print("\n====> ");
+        int delNum = Integer.parseInt(scanner.nextLine());
+        
+        if (delNum == 99) {
+            phoneBook.remove(name);}
+        else {
+            phoneBook.get(name).remove(delNum);
+        }
+
+        } catch (Exception e) {
+            System.out.printf("\nОшибка ввода (%s)",e);
+            System.out.println("\nНажмите чтобы продолжить...");
+            Scanner scanner = new Scanner(System.in);
+            scanner.nextLine();
+            return;
+        }
+    }
+    public static void clear(){
+        System.out.print("\033[H\033[2J");  
+        System.out.flush(); 
+    }
+    public static void exit() throws InterruptedException{
+        clear();
+        System.out.println("Завершение программы...");
+        for (int i = 0; i < 5; i++) {
+            System.out.print("= ");
+            Thread.sleep(500, 0);
+        } clear();System.exit(0);
+    }
+}
---
+++ LeanHashMap.java
@@ -0,0 +1,81 @@
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
+
+public class LeanHashMap {
+    public static void main(String[] args) {
+        sortLenghtOptimized();
+    }
+    public static void exPass(){
+
+        Map<String, String> workers = new HashMap<>();
+        workers.put("100", "Ivanov");
+        workers.put("101", "Petrov");
+        workers.put("102", "Sidorov");
+        workers.put("103", "Sosnov");
+        workers.put("104", "Evalion");
+        workers.put("105", "Kalipos");
+
+        for (Map.Entry<String, String> worker : workers.entrySet()) {
+            System.out.printf("Num passport: %s, Worker: %s\n", worker.getKey(), worker.getValue());
+        }
+    }
+
+    public static void rimToAraab(){
+        Map<Character, Integer> rimNums = new HashMap<>();
+        rimNums.put('I', 1);
+        rimNums.put('V', 5);
+        rimNums.put('X', 10);
+        rimNums.put('L', 50);
+        rimNums.put('C', 100);
+        rimNums.put('D', 500);
+        rimNums.put('M', 1000);
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите число: ");
+        char[] romeNumber = scanner.nextLine().toUpperCase().toCharArray();
+        int result = 0;
+        scanner.close();
+
+        for (int i = 0; i < romeNumber.length; i++) {
+            if (i != romeNumber.length -1 && rimNums.get(romeNumber[i]) < rimNums.get(romeNumber[i+1])) {
+                result+= rimNums.get(romeNumber[i+1]) - rimNums.get(romeNumber[i]);
+                i++;
+             } else { 
+                result+=rimNums.get(romeNumber[i]); 
+            }
+        } System.out.println("Результат: " + result);
+    }
+
+    public static void sortLenght(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите строку: ");
+        String[] words = scanner.nextLine().split(" ");
+        
+
+        Map<Integer, String> sortedWords = new TreeMap<>();
+        for (String word : words) {
+            sortedWords.put(word.length(), word);        }
+        System.out.println(sortedWords);
+        scanner.close();
+    }
+
+    public static void sortLenghtOptimized(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите строку: ");
+        String[] words = scanner.nextLine().split(" ");
+
+        Map<Integer,LinkedList<String>> sortedWords = new TreeMap<>();
+
+        for (String word : words) {
+            if(sortedWords.containsKey(word.length())){
+                sortedWords.get(word.length()).add(word);}
+            else {
+                sortedWords.put(word.length(), new LinkedList<>());
+                sortedWords.get(word.length()).add(word);}}
+        System.out.println(sortedWords);
+        scanner.close();
+    }
+}
